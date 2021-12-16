86215eec65021ea1049ab38b66b0a97615526ddd
avi.13 <avicekbhatta.123@gmail.com>
2021-12-16 08:10:51 +0545

    Day5

--- Day3/lab4.py
+++ Day3/lab4.py
@@ -1,4 +1,4 @@
 # Given an integer number, print its last digit.
 a =int(input ("Enter a integer "))
-l = a%10
-print ("the last digit of your num is:\t",l)
+b = a%10
+print ("the last digit of your num is:\t",b)
