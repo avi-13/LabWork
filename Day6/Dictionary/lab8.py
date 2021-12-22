a3b6c96b0d2139682c1afb3fde3193a9e7f0a8ed
avi.13 <avicekbhatta.123@gmail.com>
2021-12-22 22:49:48 +0545

    day6

--- Day6/Dictionary/lab8.py
+++ Day6/Dictionary/lab8.py
@@ -1,1 +1,8 @@
 # Write a Python script to merge two Python dictionaries.
+#merge two Python dictionaries
+dict={}
+dict1={1:"b",2:"a",3:"d"}
+dict2={"x":"d","y":"a","z":"y"}
+dict=dict1.copy()
+dict.update(dict2)
+print(dict)
