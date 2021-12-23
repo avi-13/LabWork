1d68224dda00459380314f544248ba713bd4255a
avi.13 <avicekbhatta.123@gmail.com>
2021-12-23 17:05:31 +0545

    day1

--- Day 1/lab1.py
+++ Day 1/lab1.py
@@ -2,5 +2,5 @@
 a = input ("enter first number:\n")
 b = input("enter 2nd number:\n")
 c = input("enter 3rd number:\n")
-d = int(a)+int(b)+int(c) 
-print ("the sum of the number is :",d)
+e = int(a)+int(b)+int(c) 
+print ("the sum of the number is :",e)
